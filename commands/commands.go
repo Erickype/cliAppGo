22a7f77474653542f14074138cc03edace9080f0
Erickype <erickype@hotmail.com>
2022-08-15 16:38:16 -0500

    Removing the "\n" character

--- commands/commands.go
+++ commands/commands.go
@@ -2,17 +2,23 @@ package commands
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
 )
 
 var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 
 func GetInput() (string, error) {
+	fmt.Print("->")
 	str, err := reader.ReadString('\n')
 
 	if err != nil {
 		return "", err
 	}
-
+	/* 	Remove using len
+	   	str = str[0 : len(str)-2] */
+	//remove using strings
+	str = strings.Replace(str, "\n", "", 1)
 	return str, nil
 }
