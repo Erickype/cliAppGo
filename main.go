19b0d6ff9f5433eba08dff303f2583fd7cf0291a
Erickype <erickype@hotmail.com>
2022-08-15 16:38:40 -0500

    Infinite loop for save expenses

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Erickype/cliAppGo/commands"
+)
+
 func main() {
 
+	for {
+		input, err := commands.GetInput()
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if input == "n" {
+			break
+		}
+	}
+	log.Println("Finalizado")
 }
