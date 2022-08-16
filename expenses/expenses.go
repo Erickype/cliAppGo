e1cca3a4236434c5eebc7dc39319a10eabb8cefb
Erickype <erickype@hotmail.com>
2022-08-16 16:00:10 -0500

    Min function

--- expenses/expenses.go
+++ expenses/expenses.go
@@ -25,3 +25,20 @@ func Max(expens ...float32) float32 {
 
 	return Sum(expens...) / float32(len(expens))
 }
+
+func Min(expens ...float32) float32 {
+
+	if len(expens) == 0 {
+		return 0
+	}
+
+	var min = expens[0]
+
+	for _, expen := range expens {
+		if expen < min {
+			min = expen
+		}
+	}
+
+	return Sum(expens...) / float32(len(expens))
+}
