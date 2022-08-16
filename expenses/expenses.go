7ca9e5fe74418009ebdbb599c7f8b30143e6f01a
Erickype <erickype@hotmail.com>
2022-08-16 15:53:35 -0500

    Function Sum for return the total

--- expenses/expenses.go
+++ expenses/expenses.go
@@ -1,11 +1,14 @@
 package expenses
 
 func Average(expens ...float32) float32 {
-	var sum float32
 
+	return Sum(expens...) / float32(len(expens))
+}
+
+func Sum(expens ...float32) float32 {
+	var sum float32
 	for _, expen := range expens {
 		sum += expen
 	}
-
-	return sum / float32(len(expens))
+	return sum
 }
