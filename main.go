ae43567f72f7f286e563922144f02478a88575c8
Erickype <erickype@hotmail.com>
2022-08-18 19:27:25 -0500

    Implementation of ShowInConsole

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/Erickype/cliAppGo/commands"
 )
 
 func main() {
+
+	var expenses []float32
 
 	for {
 		input, err := commands.GetInput()
@@ -18,6 +21,14 @@ func main() {
 		if input == "n" {
 			break
 		}
+
+		expense, err := strconv.ParseFloat(input, 32)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		expenses = append(expenses, float32(expense))
 	}
-	log.Println("Finalizado")
+
+	commands.ShowInConsole(expenses)
 }
